cb6c0e9ea3b0678be2b79b715a87973ba39020e9
jerrod <jlwelsh@gmail.com>
2018-12-05 13:54:28 -0600

    ?

--- sqs.py
+++ sqs.py
@@ -6,4 +6,5 @@ import boto3
 session = boto3.Session(profile_name='pythonAutomation')
 sqs = session.resource('sqs')
 
+"""Creates queue with a 5 second delivery delay"""
 queue = sqs.create_queue(QueueName='testqueue', Attributes={'DelaySeconds': '5'})
